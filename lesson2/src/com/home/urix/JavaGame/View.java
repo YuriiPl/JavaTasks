7957db37a04d32df2afcfd4bbae621baac340a9c
YuriiPl <YuriiPl@fake.com>
2021-08-30 21:00:59 +0300

    Fix few syntax error

--- lesson2/src/com/home/urix/JavaGame/View.java
+++ lesson2/src/com/home/urix/JavaGame/View.java
@@ -8,8 +8,8 @@ public class View {
     public static final String NOT_IN_RANGE_MESSAGE="Number isn't in the range (%1,%2)\n";
     public static final String YOUR_INPUT_MESSAGE="Your input: ";
     public static final String INCORRECT_INPUT_MESSAGE="Input is incorrect. Integer expected\n";
-    public static final String MORE_MESSAGE="My number more than %1\n";
-    public static final String LESS_MESSAGE="My number less than %1\n";
+    public static final String MORE_MESSAGE="My number is more than %1\n";
+    public static final String LESS_MESSAGE="My number is less than %1\n";
     public static final String CHANGE_LIMITS_MESSAGE="And now, the limits look like this: [%1,%2]\n";
     public static final String WIN_MESSAGE="You guessed! You're a winner! Congratulations!\n";
     public static final String PREVIOUSLY_START_MESSAGE="*** PREVIOUSLY ***\n";

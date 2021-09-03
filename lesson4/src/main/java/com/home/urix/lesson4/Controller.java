e7b69dd595291c712863367e14b1718b050d43d2
YuriiPl <YuriiPl@fake.com>
2021-09-04 02:02:49 +0300

    Refactoring + add some regexp (ua) until 10

--- lesson4/src/main/java/com/home/urix/lesson4/Controller.java
+++ lesson4/src/main/java/com/home/urix/lesson4/Controller.java
@@ -4,6 +4,7 @@ package com.home.urix.lesson4;
 
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Controller {
     private Model model;
@@ -14,23 +15,56 @@ public class Controller {
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
-        regexps = ResourceBundle.getBundle("regexps", view.currentLocale());
         scanner = new Scanner(System.in);
     }
 
     public void processRegistration(){
-        view.inputLastNameMessage();
-        String userInput;
-        while(checkLastName(userInput=scanner.nextLine()));
-        model.setSetLastName(userInput);
+//        view.inputLastNameMessage();
+//        String userInput;
+//        while(checkLastName(userInput=scanner.nextLine()));
+//        model.setSetLastName(userInput);
+
 
     }
 
     boolean checkLastName(String userInput){
-        return userInput.matches(regexps.getString("check.string.regexp.lastname"));
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.lastname"));
     }
 
     boolean checkFirstName(String userInput){
-        return userInput.matches(regexps.getString("check.string.regexp.firstname"));
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.firstname"));
     }
+
+    boolean checkMidName(String userInput){
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.midname"));
+    }
+
+    boolean checkNickName(String userInput){
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.nickname"));
+    }
+
+    boolean checkComment(String userInput){
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.comment"));
+    }
+
+    boolean checkHomePhone(String userInput){
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.homephonenumber"));
+    }
+
+    boolean checkMobilePhone(String userInput){
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.homemobilenumber"));
+    }
+
+    boolean checkMobilePhone2(String userInput){
+        if(userInput.length()==0)return true;
+        return userInput.matches(TextFactory.getRegExpString("check.string.regexp.homemobilenumber"));
+    }
+
+    GroupsEnum toGroup(String userInput){
+        if (TextFactory.getRegExpString("check.string.regexp.group.admin").equals(userInput)) return GroupsEnum.ADMIN;
+        if (TextFactory.getRegExpString("check.string.regexp.group.manager").equals(userInput)) return GroupsEnum.MANAGER;
+        if (TextFactory.getRegExpString("check.string.regexp.group.user").equals(userInput)) return GroupsEnum.USER;
+        return GroupsEnum.ERROR_GROUP;
+    }
+
 }
---
+++ lesson4/src/main/java/com/home/urix/lesson4/TextFactory.java
@@ -0,0 +1,25 @@
+package com.home.urix.lesson4;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class TextFactory {
+    static private ResourceBundle messages;
+    static private ResourceBundle regexps;
+    static {
+        messages = ResourceBundle.getBundle("messages");
+        regexps = ResourceBundle.getBundle("regexps");
+    }
+    static public void changeLocale(Locale locale){
+        messages = ResourceBundle.getBundle("messages",locale);
+        regexps = ResourceBundle.getBundle("regexps",locale);
+    }
+
+    static public String getString(String str){
+        return messages.getString(str);
+    }
+
+    static public String getRegExpString(String str){
+        return regexps.getString(str);
+    }
+}

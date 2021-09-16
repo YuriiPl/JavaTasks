88e16a56bac1ccb62dd498307904913ba00790e6
YuriiPl <YuriiPl@fake.com>
2021-09-16 21:25:34 +0300

    Release

--- lesson7/src/com/home/urix/lesson7/Main.java
+++ lesson7/src/com/home/urix/lesson7/Main.java
@@ -3,8 +3,13 @@ package com.home.urix.lesson7;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Shape shape = new Rectangle(new Point(3,4),new Point(7,6));
+        System.out.println(shape);
+
+        shape = new Circle(new Point(3,4),7.4);
+        System.out.println(shape);
+
+        shape = new Triangle(new Point(3,4),new Point(7,-6),new Point(-7,6));
         System.out.println(shape);
     }
 }

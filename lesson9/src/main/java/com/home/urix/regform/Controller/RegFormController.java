4cbae0b9e0d722406eb750fb4c320baf0078e606
YuriiPl <YuriiPl@fake.com>
2021-10-03 01:29:11 +0300

    Fix validators

--- lesson9/src/main/java/com/home/urix/regform/Controller/RegFormController.java
+++ lesson9/src/main/java/com/home/urix/regform/Controller/RegFormController.java
@@ -46,12 +46,6 @@ public class RegFormController {
         return new ResponseEntity<>(Collections.singletonMap("message",Collections.singleton("user_email_exist")), HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String,Set<String>>> handleIllegalArgumentException(IllegalArgumentException ex){
-        log.error("{} {}", ex.getClass(), ex.getLocalizedMessage());
-        return new ResponseEntity<>(Collections.singletonMap("message",Collections.singleton("somethingWrong")), HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Map<String,Set<String>>> handleRuntimeException(ConstraintViolationException ex) {
         log.warn("{} {}", ex.getClass(), ex.getLocalizedMessage());

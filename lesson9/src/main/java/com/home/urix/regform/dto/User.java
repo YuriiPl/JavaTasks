ff65298aeb81cdbe6bdad915f341c6e94cab4910
YuriiPl <YuriiPl@fake.com>
2021-10-04 03:48:53 +0300

    BugFixing (Apostrophe in Ukrainian i18n)

--- lesson9/src/main/java/com/home/urix/regform/dto/User.java
+++ lesson9/src/main/java/com/home/urix/regform/dto/User.java
@@ -16,23 +16,15 @@ import javax.validation.constraints.*;
 @Builder
 @ToString
 public class User {
-    @NotBlank
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,24}$", message = "wrongPassword")
     private String password;
 
-    @Pattern(regexp = "^(?! )(?!.* $)(?!(?:.* ){2})[a-zA-Zа-щьюяіїєґА-ЩЮЯІЇЄҐ ']{2,}$", message = "wrongUsername")
     private String name;
 
-    @Email(message = "wrongEmail")
     private String email;
 
-    @Pattern(regexp = "^[\\w\\d]+$", message = "wrongLogin")
-    @Size(min = 6, max = 24, message = "wrongLoginSize")
     private String login;
 
     private boolean acceptNewsLatter;
 
-    @NotNull(message = "wrongGender")
-    @Enumerated(EnumType.STRING)
     private UserSex userSex;
 }
--- lesson9/src/main/java/com/home/urix/regform/entity/UserDto.java
+++ lesson9/src/main/java/com/home/urix/regform/entity/UserDto.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 
 @Getter
@@ -13,7 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Builder
 @ToString
-@Entity // This tells Hibernate to make a table out of this class
+@Entity(name = "user") // This tells Hibernate to make a table out of this class
 public class UserDto {
 
     public UserDto(User user){
@@ -29,21 +30,29 @@ public class UserDto {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     long id;
 
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,24}$", message = "wrongPassword")
     @Column(name = "passwd")
     private String password;
 
+    @Pattern(regexp = "^(?! )(?!.* $)(?!(?:.* ){2})[a-zA-Zа-щьюяіїєґА-ЩЮЯІЇЄҐ ']{2,}$", message = "wrongUsername")
     @Column(name = "username")
     private String name;
 
+    @Email(message = "wrongEmail")
     @Column(name = "mail", unique = true)
     private String email;
 
+    @Pattern(regexp = "^[\\w\\d]+$", message = "wrongLogin")
+    @Size(min = 6, max = 24, message = "wrongLoginSize")
     @Column(name = "userlogin", unique = true)
     private String login;
 
     @Column(name = "newsaccept")
     private boolean acceptNewsLatter;
 
+    @NotNull(message = "wrongGender")
+    @Enumerated(EnumType.STRING)
     @Column(name = "sex")
     private UserSex userSex;
 
